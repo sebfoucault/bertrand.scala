c47626469867b8302dbed8d9ba1927a958b84320
sebastien foucault <sebastien.foucault@gmail.com>
2015-09-11 21:29:40 +0200

    Improve the implementation

--- Bertrand.scala
+++ Bertrand.scala
@@ -16,13 +16,22 @@ class OpInstance(op1: Int, op2: Int, opr: Op) {
     override def toString = op1.toString + opr.sym + op2 + '=' + opr(op1, op2) 
 }
 
-case class Problem(target: Int, values: List[Int]) {
+case class Challenge(target: Int, values: List[Int]) {
     def solved = values.contains(target)
 }
 
-case class ProblemAndOps(problem: Problem, operations: List[OpInstance])
+case class ChallengeAndOps(problem: Challenge, operations: List[OpInstance])
 
 class Solver {
+
+    def iRemoveDuplicates[T,X](l: List[T], f: Set[X], k: (T) => X): List[T] = l match {
+        case Nil => Nil
+        case x :: xs => if (f.contains(k(x))) iRemoveDuplicates(xs, f, k) else x :: iRemoveDuplicates(xs, f+k(x), k)
+    }
+
+    def removeDuplicates[T,X](l: List[T], k: (T) => X): List[T] = {
+        iRemoveDuplicates(l, Set(), k)
+    }
 
     def extractAt[T](l: List[T], i: Int, j: Int) = {
         val z = l.indices zip l
@@ -44,31 +53,31 @@ class Solver {
         pairComb(l, !op.commutative).toList.filter(tester).map(mapper)
     }
 
-    def recombine(pnop: ProblemAndOps, combs: List[(List[Int],OpInstance)]): List[ProblemAndOps] = combs match {
-        case x :: xs => ProblemAndOps(Problem(pnop.problem.target, x._1), x._2 :: pnop.operations) :: recombine(pnop, xs)
+    def recombine(pnop: ChallengeAndOps, combs: List[(List[Int],OpInstance)]): List[ChallengeAndOps] = combs match {
+        case x :: xs => ChallengeAndOps(Challenge(pnop.problem.target, x._1), x._2 :: pnop.operations) :: recombine(pnop, xs)
         case Nil => Nil
     }
 
-    def deriveProblemByOp(pnop: ProblemAndOps, op: Op) = pnop match {
-        case ProblemAndOps(Problem(_, pbValues @ List(_,_, _*)), _) => recombine(pnop, opComb(pbValues, op)) 
+    def deriveChallengeByOp(pnop: ChallengeAndOps, op: Op) = pnop match {
+        case ChallengeAndOps(Challenge(_, pbValues @ List(_,_, _*)), _) => recombine(pnop, opComb(pbValues, op)) 
         case _ => Nil
     }
 
-    def deriveProblemsByOps(pnop: ProblemAndOps, ops: List[Op]): List[ProblemAndOps] = ops match {
+    def deriveChallengesByOps(pnop: ChallengeAndOps, ops: List[Op]): List[ChallengeAndOps] = ops match {
         case Nil => Nil
-        case x :: xs => deriveProblemByOp(pnop, x) ::: deriveProblemsByOps(pnop, xs)
+        case x :: xs => removeDuplicates(deriveChallengeByOp(pnop, x) ::: deriveChallengesByOps(pnop, xs), (x: ChallengeAndOps) => x.problem.values.sorted)
     }
 
-    def solve(pnop: ProblemAndOps) = {
-        if (pnop.problem.solved) Some(pnop.operations.reverse) else doSolve(deriveProblemsByOps(pnop, Op.all))
+    def solve(pnop: ChallengeAndOps) = {
+        if (pnop.problem.solved) Some(pnop.operations.reverse) else doSolve(deriveChallengesByOps(pnop, Op.all))
     }
 
-    def doSolve(pnops: List[ProblemAndOps]): Option[List[OpInstance]] = pnops match {
+    def doSolve(pnops: List[ChallengeAndOps]): Option[List[OpInstance]] = pnops match {
         case x :: xs => solve(x) orElse doSolve(xs)
         case _ => None
     }
 
-    def solve(p: Problem) = {
-        doSolve( ProblemAndOps(p,Nil) :: Nil)
+    def solve(p: Challenge) = {
+        doSolve( ChallengeAndOps(p,Nil) :: Nil)
     }
 }
