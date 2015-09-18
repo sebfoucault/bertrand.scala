cc72dab62470a858f406f445a8a7e768a8f90216
sebastien foucault <sebastien.foucault@gmail.com>
2015-09-18 23:06:22 +0200

    Improve performance

--- Bertrand.scala
+++ Bertrand.scala
@@ -6,10 +6,10 @@ final class Op(val sym: Char, val commutative: Boolean, etor: (Int,Int) => Int, 
 }
 
 object Op {
-    val add = new Op('+', true, (x: Int, y: Int) => x+y, null)
-    val sub = new Op('-', false, (x: Int, y: Int) => x-y, (x: Int, y: Int) => x > y)
-    val mul = new Op('*', true, (x: Int, y: Int) => x*y, null)
-    val div = new Op('/', false, (x: Int, y: Int) => x/y, (x: Int, y: Int) => (x % y == 0 && y != 0))
+    private val add = new Op('+', true, (x: Int, y: Int) => x+y, null)
+    private val sub = new Op('-', false, (x: Int, y: Int) => x-y, (x: Int, y: Int) => x > y)
+    private val mul = new Op('*', true, (x: Int, y: Int) => x*y, (x: Int, y: Int) => x != 1 && y != 1)
+    private val div = new Op('/', false, (x: Int, y: Int) => x/y, (x: Int, y: Int) => (x % y == 0 && y != 0 && y != 1))
     val all = List(Op.add, Op.sub, Op.mul, Op.div)
 }
 
@@ -25,59 +25,43 @@ case class ChallengeAndOps(problem: Challenge, operations: List[OpInstance])
 
 final class Solver {
 
-    def unique[T,X](l: List[T], k: (T) => X): List[T] = {
-        l.groupBy(k).map{ case(key,value) => value.head }.toList
-    }
+    def unique[T,X](l: List[T], k: (T) => X) = l.groupBy(k).map{ case(key,value) => value.head }.toList
 
-    def uniqueChallengeAndOps = unique( _:List[ChallengeAndOps], (x: ChallengeAndOps) => x.problem.values.sorted)
+    def uniqueChallengeAndOps = unique( _:List[ChallengeAndOps], (x: ChallengeAndOps) => x.problem.values)
 
-    def extractAt[T](l: List[T], i: Int, j: Int) = {
+    def extractAt(l: List[Int], i: Int, j: Int) = {
         val z = l.indices zip l
-        val a = z.filter(_._1==i).head._2
-        val b = z.filter(_._1==j).head._2
-        val r = z.filter( (t) => (t._1 != i && t._1 != j)).map(_._2).toList 
-        ((a,b), r)
+        val r = z.groupBy( (x:(Int,Int)) => if (x._1 == i) 0 else if (x._1 == j) 1 else 2) 
+        ((r(0).head._2,r(1).head._2), (r.get(2) orElse Some(Nil)).get.unzip._2.toList)
     }
 
-    def pairComb[A](l:List[A], commute: Boolean) = {
+    def pairComb(l:List[Int], commute: Boolean) = 
         for (i <- 0 to l.length - 1 ; j <- 0 to l.length - 1 ; if (i != j && (commute || i < j))) yield {
             extractAt(l, i, j)
         }
-    }
 
     def opComb(l:List[Int], op: Op):List[(List[Int], OpInstance)] = {
         val tester = (x:((Int,Int),List[Int])) => op.validate(x._1._1, x._1._2)
-        val mapper = (x:((Int,Int),List[Int])) => (op(x._1._1, x._1._2) :: x._2, new OpInstance(x._1._1, x._1._2, op))
-        pairComb(l, !op.commutative).toList.filter(tester).map(mapper)
+        val mapper = (x:((Int,Int),List[Int])) => ((op(x._1._1, x._1._2) :: x._2).sorted, new OpInstance(x._1._1, x._1._2, op))
+        pairComb(l, !op.commutative).toList.collect( { case x if (tester(x)) => mapper(x) } )
     }
 
-    def recombine(pnop: ChallengeAndOps, combs: List[(List[Int],OpInstance)]): List[ChallengeAndOps] = {
-        val r = combs.map( (cmb:(List[Int],OpInstance)) => ChallengeAndOps(Challenge(pnop.problem.target, cmb._1), cmb._2 :: pnop.operations) )
-        uniqueChallengeAndOps(r)   
-    }
+    def recombine(pnop: ChallengeAndOps, combs: List[(List[Int],OpInstance)]) = 
+        combs.map( (cmb:(List[Int],OpInstance)) => ChallengeAndOps(Challenge(pnop.problem.target, cmb._1), cmb._2 :: pnop.operations) ) 
 
     def deriveChallengeByOp(pnop: ChallengeAndOps, op: Op) = pnop match {
         case ChallengeAndOps(Challenge(_, pbValues @ List(_,_, _*)), _) => recombine(pnop, opComb(pbValues, op)) 
         case _ => Nil
     }
 
-    def deriveChallengesByOps(pnop: ChallengeAndOps, ops: List[Op]): List[ChallengeAndOps] = {
-        val r = ops.map((x:Op)=>deriveChallengeByOp(pnop,x)).flatten
-        uniqueChallengeAndOps(r)
-    }
+    def deriveChallengesByOps(pnop: ChallengeAndOps, ops: List[Op]) = ops.map((x:Op)=>deriveChallengeByOp(pnop,x)).flatten
 
     @tailrec
-    def doSolve(pnops: List[ChallengeAndOps]): Option[List[OpInstance]] = pnops match {
-        case x :: xs => {
-            if (x.problem.solved) 
-                Some(x.operations.reverse) 
-            else 
-                doSolve(uniqueChallengeAndOps(xs ::: deriveChallengesByOps(x, Op.all)))
-        }   
+    def doSolve(pnops: Seq[ChallengeAndOps]): Option[Seq[OpInstance]] = pnops match {
+        case x :: xs if (x.problem.solved) =>  Some(x.operations.reverse)
+        case x :: xs =>  doSolve(uniqueChallengeAndOps(xs ::: deriveChallengesByOps(x, Op.all)))
         case _ => None
     }
 
-    def solve(p: Challenge) = {
-        doSolve( ChallengeAndOps(p,Nil) :: Nil)
-    }
+    def solve(p: Challenge) = doSolve( ChallengeAndOps(Challenge(p.target, p.values.sorted),Nil) :: Nil)
 }
